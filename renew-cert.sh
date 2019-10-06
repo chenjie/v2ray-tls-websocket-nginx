9524f9a13bc23567fd89a7f864b3c2aa624ebc3f
Chenjie Ni <chenjie@crowdmark.com>
2019-10-06 14:13:55 +0000

    Fix commit avatar

--- renew-cert.sh
+++ renew-cert.sh
@@ -12,4 +12,5 @@ systemctl stop nginx
 ~/.acme.sh/acme.sh --renew -d $do_name --ecc
 ~/.acme.sh/acme.sh --installcert -d $do_name --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 systemctl start nginx
-echo "SSL cert renew done"
+echo
+echo "SSL cert renew is done!"
